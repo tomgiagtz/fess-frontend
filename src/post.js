8bc535e424371ea9d7181a6c456eaede4f607e5f
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-11-13 15:35:25 -0500

    likes are a total pain, but they work now. Some insignificant bugs with updating id on new votes

--- src/post.js
+++ src/post.js
@@ -1,23 +1,27 @@
 class Post {
 
-	constructor(content, time, likes, id) {
+	constructor(content, time, likeCount, likes, id) {
 		this.content = content;
 		this.time = new Date(time);
-		this.likes = likes;
+		this.likeCount = likeCount;
+		this.likes = likes
 		this.id = id;
+
+		this.currVote = this.getVote(getUserId())
+
+	}
+
+	getLikesUrl() {
+		return 'http://localhost:3000/likes'
 	}
 
 	render() {
 		let li = document.createElement("li");
 		let p = document.createElement("p");
-		let imgUpVote = document.createElement("img");
-		let imgDownVote = document.createElement("img");
-		let p2 = document.createElement("p");
 		let span = document.createElement("span")
 		let button = document.createElement("button")
 
 		p.innerHTML = this.content;
-		p2.innerHTML = `Likes: ${this.likes}`;
 		button.innerHTML = "Comment";
 		span.innerHTML = this.time.toLocaleString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 
@@ -25,31 +29,105 @@ class Post {
 		li.className = "list-group-item";
 		button.className = "btn btn-warning";
 		span.className = "sub-text";
-		imgUpVote.src = "img/upVote.png";
-		imgDownVote.src = "img/downVote.png";
 
-		imgUpVote.addEventListener('click', e => this.createLike(getUser(), this.id, true));
-		imgDownVote.addEventListener('click', e => this.createLike(getUser(), this.id, false));
 		button.addEventListener('click', showCommentForm)
+
+		let voteDiv = this.renderVoting()
 
 		li.appendChild(p);
 		li.appendChild(span)
-		li.innerHTML += `<br>Likes: ${this.likes}    `;
-		li.appendChild(imgUpVote);
-		li.appendChild(imgDownVote);
+		li.appendChild(voteDiv)
 		li.appendChild(button);
 		return li;
 	}
 
-	createLike(userId, postId, upvote) {
-		console.log(getUser(), this.id, upvote)
+	createVote(userId, upvote) {
+
+		if (this.currVote.vote === null) {
+			console.log('create')
+			fetch(this.getLikesUrl(), {
+				method: 'post',
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept': 'application/json'
+				},
+				body: JSON.stringify({
+					like: {
+						user_id: userId,
+						post_id: this.id,
+						upvote: upvote
+					}
+				})
+			}).then(resp => this.renderVote(upvote ? 1 : -1))
+			this.currVote.vote = upvote
+		} else if (this.currVote.vote === !upvote) {
+			console.log('put')
+			fetch('http://localhost:3000/likes/' + this.currVote.id, {
+				body: JSON.stringify({ id: this.currVote.id }),
+				method: 'put',
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept': 'application/json'
+				}
+			}).then(resp => this.renderVote(upvote ? 2 : -2))
+			this.currVote.vote = upvote
+		} else {
+			console.log('delete')
+			fetch('http://localhost:3000/likes/' + this.currVote.id, {
+				method: 'delete'
+			}).then(resp => this.renderVote(upvote ? -1 : 1))
+			this.currVote.vote = null;
+
+		}
+	}
+
+	renderVote(diff) {
+		let voteLabel = document.querySelector('#likes-' + this.id)
+		voteLabel.innerText = parseInt(voteLabel.innerText) + diff
+	}
+	//returns true for upvote, false for downvote and null for no vote
+	getVote(userId) {
+		let res = {
+			vote: null,
+			likeId: null
+		}
+
+		this.likes.forEach(like => {
+			if (like.user_id === userId) {
+				res.vote = like.upvote
+				res.id = like.id
+			}
+		})
+		return res
+	}
+
+	renderVoting() {
+		let voteDiv = document.createElement("div")
+
+		let upVoteBtn = document.createElement("img");
+		upVoteBtn.src = "img/upVote.png";
+		upVoteBtn.addEventListener('click', e => this.createVote(getUserId(), true));
+
+		let downVoteBtn = document.createElement("img");
+		downVoteBtn.src = "img/downVote.png";
+		downVoteBtn.addEventListener('click', e => this.createVote(getUserId(), false));
+
+		let likeLabel = document.createElement("label");
+		likeLabel.innerHTML = `${this.likeCount}`;
+		likeLabel.id = 'likes-' + this.id
+
+		voteDiv.appendChild(upVoteBtn)
+		voteDiv.appendChild(likeLabel)
+		voteDiv.appendChild(downVoteBtn)
+
+		return voteDiv
 
 	}
 
 	static renderPosts(posts) {
 		let container = document.getElementById("post-container");
 		posts.forEach(post => {
-			let newPost = new Post(post.content, post.created_at, post.like_count, post.id)
+			let newPost = new Post(post.content, post.created_at, post.like_count, post.likes, post.id)
 			container.appendChild(newPost.render());
 		})
 	}
